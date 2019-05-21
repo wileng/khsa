f1df3890a51918fc0bac70ef1dad2032828575b1
William Eng <william_eng@outlook.com>
2019-05-21 00:09:19 -0700

    swapped nav abr from black to transperant

--- style.js
+++ style.js
@@ -6,8 +6,8 @@ var link = document.getElementById("link");
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (currentScrollPos <= 50) {
-    nav.style.backgroundColor = "transparent";
-  } else {
     nav.style.backgroundColor = "black";
+  } else {
+    nav.style.backgroundColor = "transperant";
   }
 }
