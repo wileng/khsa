2d2691d781dff5b2a99f3363308363cc147ee537
William Eng <william_eng@outlook.com>
2019-05-21 00:51:43 -0700

    made minor page changes

--- style.js
+++ style.js
@@ -1,13 +1,12 @@
 
 var prevScrollpos = window.pageYOffset;
 var nav = document.getElementById("navbar");
-var link = document.getElementById("link");
 
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (currentScrollPos <= 50) {
     nav.style.backgroundColor = "black";
   } else {
-    nav.style.backgroundColor = "transparent";
+    nav.style.backgroundColor = "black";
   }
 }
